7203418e6f36d380fd08ea02070c7a2016c9c9a4
Alvaro Valdez <v.alvaro.r@gmail.com>
2025-05-29 20:37:01 -0400

    fix(auth): se añade importación de middleware authenticate

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
-
 const router = express.Router();
+const { authenticate } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
